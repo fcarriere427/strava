310d8ca4e05b5164c5b49fb318d78d9748b4a224
florian <florian.carriere@gmail.com>
2025-01-01 22:58:10 +0100

    add one

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,5 +3,5 @@ export { default as Footer } from "./Footer";
 export { default as Tracker } from "./Tracker";
 export { default as List } from "./List";
 export { default as Report } from "./Report";
-export { default as MapALl } from "./Map";
+export { default as MapALl } from "./MapAll";
 export { default as Activity } from "./Activity";
