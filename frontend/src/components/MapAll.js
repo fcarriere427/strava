4a912057e70ab1d133c358142b959f3b9061af01
florian <florian.carriere@gmail.com>
2025-01-01 23:23:20 +0100

    correct MapAll

--- frontend/src/components/MapAll.js
+++ frontend/src/components/MapAll.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
+import { SelectYear } from './List/SelectYear'
 import 'leaflet/dist/leaflet.css';
 import axios from 'axios';
 
@@ -7,11 +8,12 @@ const polyUtil = require('../utils/polylineFunctions.js');
 
 const MapAll = ({year}) => {
   const [traces, setTraces] = useState([]);
+  const [currentYear, setCurrentYear] = useState("all");
 
   useEffect(() => {
     const url = year === "all" 
       ? '/api/strava/activities_list' 
-      : `/api/strava/activities_list?year=${year}`;
+      : `/api/strava/activities_list?year=${currentYear}`;
       
     axios.get(url)
       .then(response => {
@@ -23,28 +25,36 @@ const MapAll = ({year}) => {
           }));
         setTraces(activitiesWithTraces);
       });
-  }, [year]);
+  }, [currentYear]);
 
   return (
-    <MapContainer 
-      center={[48.8534, 2.3488]} 
-      zoom={13} 
-      style={{ height: "600px", width: "100%" }}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; OpenStreetMap contributors'
-      />
-      {traces.map(trace => (
-        <Polyline 
-          key={trace.id}
-          positions={trace.trace}
-          color="red"
-          weight={3}
-          opacity={0.5}
-        />
-      ))}
-    </MapContainer>
+    <Container fluid className='bg-grey text-black text-center'>
+        <Row className="py-2">
+          <SelectYear 
+            currentYear={currentYear} 
+            updateHandler={(year) => setCurrentYear} />
+        </Row>
+
+        <MapContainer 
+          center={[48.8534, 2.3488]} 
+          zoom={13} 
+          style={{ height: "600px", width: "100%" }}
+        >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; OpenStreetMap contributors'
+            />
+            {traces.map(trace => (
+              <Polyline 
+                key={trace.id}
+                positions={trace.trace}
+                color="red"
+                weight={3}
+                opacity={0.5}
+              />
+            ))}
+        </MapContainer>
+    </Container>
   );
 };
 
