3fed24b0bad886fc1df0e2e7959e672c0a12fc35
florian <florian.carriere@gmail.com>
2025-01-02 16:57:47 +0100

    logs

--- backend/routes/reload.js
+++ backend/routes/reload.js
@@ -8,11 +8,20 @@ module.exports = {
             .get("/", (req, res) => {
               // param de getActivities = nbPages --> ici 10(*100) car 908 activités Strava le 27/12/24 (cf. dashboard Strava) --> il faut mettre la centaine supérieure, pas plus !
               var nbPages = 10;
-              dbFun.renewDB()
-              .then(() => strava.renewTokens())
-              .then(() => strava.getActivities(nbPages))
+              console.log(new Date().toISOString(),'- reload.js - Appel de l\'API "/api/strava/reload"');
+              dbFun.renewDB(() => {
+                console.log(new Date().toISOString(),'- reload.js - Recréation de la BDD');
+              })
+              .then(() => {
+                console.log(new Date().toISOString(),'- reload.js - Renouvellement tokens');
+                strava.renewTokens();
+              })
+              .then(() => {
+                console.log(new Date().toISOString(),'- reload.js - Appel de getActivities');
+                strava.getActivities(nbPages)
+              })
               .then((data) => {
-                console.log(new Date().toISOString(),'- month_distance.js - Toutes activités récupérées - OK');
+                console.log(new Date().toISOString(),'- reload.js - Toutes activités récupérées - OK');
                 res.status(200).json(data);
               })
             })
