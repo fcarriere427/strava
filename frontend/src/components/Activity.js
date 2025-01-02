20d4fbdc3c25b23a27abb340d07690119286e326
florian <florian.carriere@gmail.com>
2025-01-02 23:13:50 +0100

    correct

--- frontend/src/components/Activity.js
+++ frontend/src/components/Activity.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useNavigate, useLocation } from 'react';
 import { Container, Row, Col } from 'reactstrap'
 import { useParams } from 'react-router-dom';
 import { strTime, strDate, strSpeed } from "../utils/functions"
